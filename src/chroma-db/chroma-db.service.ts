2c0c0a6b965d4446eb6c9e95e66dbe0f35d76c43
Gabriel Vinicios <crisishd2332@gmail.com>
2023-08-30 21:56:32 -0300

    add query and change structure

--- src/chroma-db/chroma-db.service.ts
+++ src/chroma-db/chroma-db.service.ts
@@ -4,18 +4,29 @@ import { ChromaClient, TransformersEmbeddingFunction } from 'chromadb';
 @Injectable()
 export class ChromaDbService {
   client = new ChromaClient({ path: 'http://localhost:8000' });
-  async insertData({ Ids, metadatas, documents }) {
-    const embedder = new TransformersEmbeddingFunction();
-
-    const collection = await this.client.getOrCreateCollection({
+  embedder = new TransformersEmbeddingFunction();
+  collection = async () =>
+    await this.client.getOrCreateCollection({
       name: 'Lyra',
-      embeddingFunction: embedder,
+      embeddingFunction: this.embedder,
     });
 
-    await collection.add({
+  async insertData({ Ids, metadatas, documents }) {
+    await (
+      await this.collection()
+    ).add({
       ids: Ids,
       metadatas: metadatas,
       documents: documents,
+    });
+  }
+
+  async query() {
+    await (
+      await this.collection()
+    ).query({
+      nResults: 2,
+      queryTexts: ['lorem ipsum'],
     });
   }
 }
